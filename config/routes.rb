c07c61fdd4fc5e0dca4134fd8a01b32852cd560e
Ariel Lopez <ariel_lopez@ligph.com>
2021-04-15 09:55:24 +0800

    adding default routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root to: 'api/v1/posts#index'
+
   namespace :api, defaults: { format: 'json' } do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :posts, only: [:index, :show], param: :slug
