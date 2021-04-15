ac512f444a3c342361be304de68dfbc5b6dc208d
Ariel Lopez <ariel_lopez@ligph.com>
2021-04-15 10:16:47 +0800

    adding spec

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::PostsController < Api::V1::BaseController
 
   api :GET, "/posts", "Posts List"
   def index
-    render json: { result: [{ name: "post 1"}] }
+    render json: { result: [{ name: "post 1"}] }, status: 200
   end
 
 end
---
+++ spec/request/api/v1/posts/index_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Posts", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+
+      # this will perform a GET request to the /posts route
+      get "/api/posts"
+
+      # 'response' is a special object which contain HTTP response received after a request is sent
+      # response.body is the body of the HTTP response, which here contain a JSON string
+      expect(response.body).to eq('{"result":[{"name":"post 1"}]}')
+
+      # we can also check the http status of the response
+      expect(response.status).to eq(200)
+    end
+  end
+end
