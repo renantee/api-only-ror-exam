24490e10839ce8b0ba665bd48776a08d49de270f
Renante D. Entera <renante_entera@ligph.com>
2021-04-22 17:21:04 +0800

    RSpec User model with password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,11 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
+  before do
+    @password = "password"
+  end
   subject do
-    described_class.new(name: "My name", email: "email@email.com")
+    described_class.new(name: "My name", email: "11email@email.com", password: @password)
   end
 
   it "is valid with valid attributes" do
@@ -18,10 +21,17 @@ RSpec.describe User, type: :model do
     subject.email = nil
     expect(subject).to_not be_valid
   end
+
+  it "should authenticate using bcrypt" do
+    expect(subject.authenticate(@password)).to be_valid
+  end
 end
 
 RSpec.describe User, type: :model do
-  before { described_class.create!(name: "My name", email: "email@email.com") }
+  before do
+    described_class.create!(name: "My name", email: "11email@email.com", password: "password")
+  end
+
   it "is invalid if the email is not unique" do
     expect(subject).to be_invalid
   end
