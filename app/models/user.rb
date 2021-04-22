8bbb56ebebb0a508a468c90022ec8150e7373205
Renante D. Entera <renante_entera@ligph.com>
2021-04-22 15:26:27 +0800

    RSpec User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :name, :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    described_class.new(name: "My name", email: "email@email.com")
+  end
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a name" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without an email" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+end
+
+RSpec.describe User, type: :model do
+  before { described_class.create!(name: "My name", email: "email@email.com") }
+  it "is invalid if the email is not unique" do
+    expect(subject).to be_invalid
+  end
 end
