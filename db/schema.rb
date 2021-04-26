837387f32f9cf9b0c3dae11a1116bf452cf193fe
Renante D. Entera <renante_entera@ligph.com>
2021-04-26 17:07:25 +0800

    Add model Blacklist and migration
    For storage of expired/blacklisted tokens

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_23_140003) do
+ActiveRecord::Schema.define(version: 2021_04_26_090331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blacklists", force: :cascade do |t|
+    t.string "token"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string "title"
---
+++ spec/factories/blacklists.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :blacklist do
+    token { "MyString" }
+  end
+end
